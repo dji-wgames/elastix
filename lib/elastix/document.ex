5e0fdd1e35b4998b0e21337e7973a1965eb823dc
Darren Inksetter <darren@wgames.com>
2018-11-20 10:02:15 -0500

    Added update_matching

--- lib/elastix/document.ex
+++ lib/elastix/document.ex
@@ -12,17 +12,17 @@ defmodule Elastix.Document do
 
   ## Examples
 
-      iex> Elastix.Document.index("http://localhost:9200", "twitter", "tweet", "42", %{user: "kimchy", post_date: "2009-11-15T14:12:12", message: "trying out Elastix"})
-      {:ok, %HTTPoison.Response{...}}
+  iex> Elastix.Document.index("http://localhost:9200", "twitter", "tweet", "42", %{user: "kimchy", post_date: "2009-11-15T14:12:12", message: "trying out Elastix"})
+  {:ok, %HTTPoison.Response{...}}
   """
   @spec index(
-          elastic_url :: String.t(),
-          index :: String.t(),
-          type :: String.t(),
-          id :: String.t(),
-          data :: map,
-          query_params :: Keyword.t()
-        ) :: HTTP.resp()
+    elastic_url :: String.t(),
+    index :: String.t(),
+    type :: String.t(),
+    id :: String.t(),
+    data :: map,
+    query_params :: Keyword.t()
+  ) :: HTTP.resp()
   def index(elastic_url, index_name, type_name, id, data, query_params \\ []) do
     prepare_url(elastic_url, make_path(index_name, type_name, query_params, id))
     |> HTTP.put(JSON.encode!(data))
@@ -33,16 +33,16 @@ defmodule Elastix.Document do
 
   ## Examples
 
-      iex> Elastix.Document.index_new("http://localhost:9200", "twitter", "tweet", %{user: "kimchy", post_date: "2009-11-15T14:12:12", message: "trying out Elastix"})
-      {:ok, %HTTPoison.Response{...}}
+  iex> Elastix.Document.index_new("http://localhost:9200", "twitter", "tweet", %{user: "kimchy", post_date: "2009-11-15T14:12:12", message: "trying out Elastix"})
+  {:ok, %HTTPoison.Response{...}}
   """
   @spec index_new(
-          elastic_url :: String.t(),
-          index :: String.t(),
-          type :: String.t(),
-          data :: map,
-          query_params :: Keyword.t()
-        ) :: HTTP.resp()
+    elastic_url :: String.t(),
+    index :: String.t(),
+    type :: String.t(),
+    data :: map,
+    query_params :: Keyword.t()
+  ) :: HTTP.resp()
   def index_new(elastic_url, index_name, type_name, data, query_params \\ []) do
     prepare_url(elastic_url, make_path(index_name, type_name, query_params))
     |> HTTP.post(JSON.encode!(data))
@@ -53,16 +53,16 @@ defmodule Elastix.Document do
 
   ## Examples
 
-      iex> Elastix.Document.get("http://localhost:9200", "twitter", "tweet", "42")
-      {:ok, %HTTPoison.Response{...}}
+  iex> Elastix.Document.get("http://localhost:9200", "twitter", "tweet", "42")
+  {:ok, %HTTPoison.Response{...}}
   """
   @spec get(
-          elastic_url :: String.t(),
-          index :: String.t(),
-          type :: String.t(),
-          id :: String.t(),
-          query_params :: Keyword.t()
-        ) :: HTTP.resp()
+    elastic_url :: String.t(),
+    index :: String.t(),
+    type :: String.t(),
+    id :: String.t(),
+    query_params :: Keyword.t()
+  ) :: HTTP.resp()
   def get(elastic_url, index_name, type_name, id, query_params \\ []) do
     prepare_url(elastic_url, make_path(index_name, type_name, query_params, id))
     |> HTTP.get()
@@ -73,12 +73,12 @@ defmodule Elastix.Document do
   [Multi Get API](https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-multi-get.html).
   """
   @spec mget(
-          elastic_url :: String.t(),
-          query :: map,
-          index :: String.t(),
-          type :: String.t(),
-          query_params :: Keyword.t()
-        ) :: HTTP.resp()
+    elastic_url :: String.t(),
+    query :: map,
+    index :: String.t(),
+    type :: String.t(),
+    query_params :: Keyword.t()
+  ) :: HTTP.resp()
   def mget(elastic_url, query, index_name \\ nil, type_name \\ nil, query_params \\ []) do
     path =
       [index_name, type_name]
@@ -89,7 +89,7 @@ defmodule Elastix.Document do
       prepare_url(elastic_url, [path, "_mget"])
       |> HTTP.append_query_string(query_params)
 
-    # HTTPoison does not provide an API for a GET request with a body.
+      # HTTPoison does not provide an API for a GET request with a body.
     HTTP.request(:get, url, JSON.encode!(query))
   end
 
@@ -98,16 +98,16 @@ defmodule Elastix.Document do
 
   ## Examples
 
-      iex> Elastix.Document.delete("http://localhost:9200", "twitter", "tweet", "42")
-      {:ok, %HTTPoison.Response{...}}
+  iex> Elastix.Document.delete("http://localhost:9200", "twitter", "tweet", "42")
+  {:ok, %HTTPoison.Response{...}}
   """
   @spec delete(
-          elastic_url :: String.t(),
-          index :: String.t(),
-          type :: String.t(),
-          id :: String.t(),
-          query_params :: Keyword.t()
-        ) :: HTTP.resp()
+    elastic_url :: String.t(),
+    index :: String.t(),
+    type :: String.t(),
+    id :: String.t(),
+    query_params :: Keyword.t()
+  ) :: HTTP.resp()
   def delete(elastic_url, index_name, type_name, id, query_params \\ []) do
     prepare_url(elastic_url, make_path(index_name, type_name, query_params, id))
     |> HTTP.delete()
@@ -118,11 +118,11 @@ defmodule Elastix.Document do
   [Delete By Query API](https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-delete-by-query.html).
   """
   @spec delete_matching(
-          elastic_url :: String.t(),
-          index :: String.t(),
-          query :: map,
-          query_params :: Keyword.t()
-        ) :: HTTP.resp()
+    elastic_url :: String.t(),
+    index :: String.t(),
+    query :: map,
+    query_params :: Keyword.t()
+  ) :: HTTP.resp()
   def delete_matching(elastic_url, index_name, %{} = query, query_params \\ []) do
     prepare_url(elastic_url, [index_name, "_delete_by_query"])
     |> HTTP.append_query_string(query_params)
@@ -134,21 +134,40 @@ defmodule Elastix.Document do
 
   ## Examples
 
-      iex> Elastix.Document.update("http://localhost:9200", "twitter", "tweet", "42", %{user: "kimchy", message: "trying out Elastix.Document.update/5"})
-      {:ok, %HTTPoison.Response{...}}
+  iex> Elastix.Document.update("http://localhost:9200", "twitter", "tweet", "42", %{user: "kimchy", message: "trying out Elastix.Document.update/5"})
+  {:ok, %HTTPoison.Response{...}}
   """
   @spec update(
-          elastic_url :: String.t(),
-          index :: String.t(),
-          type :: String.t(),
-          id :: String.t(),
-          data :: map,
-          query_params :: Keyword.t()
-        ) :: HTTP.resp()
+    elastic_url :: String.t(),
+    index :: String.t(),
+    type :: String.t(),
+    id :: String.t(),
+    data :: map,
+    query_params :: Keyword.t()
+  ) :: HTTP.resp()
   def update(elastic_url, index_name, type_name, id, data, query_params \\ []) do
     elastic_url
     |> prepare_url(make_path(index_name, type_name, query_params, id, "_update"))
     |> HTTP.post(JSON.encode!(data))
+  end
+
+  @spec update_matching(
+    elastic_url :: String.t(),
+    index :: String.t(),
+    data :: map,
+    query :: map,
+    query_params :: Keyword.t()
+  ) :: HTTP.resp()
+  def update_matching(
+    elastic_url,
+    index_name,
+    %{} = data,
+    %{} = query,
+    query_params \\ []
+  ) do
+    prepare_url(elastic_url, [index_name, "_update_by_query"])
+    |> HTTP.append_query_string(query_params)
+    |> HTTP.post(JSON.encode!(Map.merge(query, data)))
   end
 
   @doc false
